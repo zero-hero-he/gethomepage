b5db3f174c1f0b24d37c566dffc75c700779d379
default <hememe2016@outlook.com>
2025-07-06 10:11:37 +0800

    Symbol correction

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     unoptimized: true,
   },
   i18n,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
 module.exports = nextConfig;
